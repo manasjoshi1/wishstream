e05b951a646b9a281f2fd2a97d9e01bf1d4a5995
Manas Joshi <joshimanassunil@gmail.com>
2025-01-30 10:53:17 -0600

    updated files

--- src/main/java/com/wishstream/core/repository/UserUploadLogRepo.java
+++ src/main/java/com/wishstream/core/repository/UserUploadLogRepo.java
@@ -1,4 +1,9 @@
-package com.wishstream.core.repository;
-
-public class UserUploadLogRepo {
-}
+//package com.wishstream.core.repository;
+//
+//import com.wishstream.core.model.UserUploadLog;
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface UserUploadLogRepo extends CrudRepository<UserUploadLog, Long > {
+//    // This class is empty because it only needs to use the methods from CrudRepository
+//    // to interact with the UserUploadLog entity.
+//}
